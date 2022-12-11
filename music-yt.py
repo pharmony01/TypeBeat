f10b824448c980767a138f338629afdf7b5cf85d
Riley Peters <rileypeters2016@gmail.com>
2022-12-10 20:23:21 -0500

    Please work

--- music-yt.py
+++ music-yt.py
@@ -4,6 +4,23 @@ from discord.ext import commands
 import wavelink
 import os
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    client.loop.create_task(connect_nodes()) # HTTPS and websocket operations
+    
+async def connect_nodes():
+    await client.wait_until_ready()
+    await wavelink.NodePool.create_node(
+        bot=client,
+        host='0.0.0.0',
+        port=2333,
+        password='youshallnotpass'
+    )
+    
+@client.event
+async def on_wavelink_node_ready(node: wavelink.Node):
+    print(f'Node: <{node.identifier}> is ready')
 
 client.run(os.environ['bot_token'])
